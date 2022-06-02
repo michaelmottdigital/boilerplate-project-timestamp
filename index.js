2027a8b36e5e8c4893e93f3ff647a02ef31a95df
Michael Mott <michael@mott.digital>
2022-06-02 11:15:45 -0400

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const res = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +25,49 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res) => {
+  var date = new Date()
+  result = {
+    unix: date.valueOf(),
+    utc: date.toUTCString()
+  }
+  res.json(result)
+})
+
+app.get("/api/:date", (req, res) => {
+  // date cam be formatted as 
+  // 1. 2015-10-01
+  // 2. a unix time stamp ie: 1451001600000
+  var invalidDate = false
+
+  date = new Date(req.params.date)
+  if (date == 'Invalid Date' ) {
+    // it might be a unix timestamp
+    date = new Date(Number(req.params.date))
+    if ( date == 'Invalid Date') {
+      // we have an invalid date 
+      // send error to user
+      invalidDate = true
+    }
+  }
+  var result;
+
+  if (invalidDate) {
+    result = {error: "Invalid Date"}
+  } else {
+    result = {
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    }
+  }
+  
+ 
+
+  res.json(result)
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(8080, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
